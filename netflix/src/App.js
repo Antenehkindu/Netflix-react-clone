d10656dbb3fcacda1e8950bf5fe90b5eca408324
Anteneh kindu <126372714+Antenehkindu@users.noreply.github.com>
2023-06-16 05:21:30 +0300

    third

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -7,10 +7,14 @@ function App() {
   return (
     <div className="App">
       <h1>Evangadi Familly</h1>
-      <Row title ="NETFLIX ORIGINALS" 
-      fetchUrl={requests.fetchNetflixOriginals} />
-      <Row title ="Trending Now" 
-      fetchUrl ={requests.fetchTrending} />
+      <Row title ="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title ="Trending Now" fetchUrl ={requests.fetchTrending} />
+      <Row title ="Top Rated" fetchUrl ={requests.fetchTopRatedMovies} />
+      <Row title ="Action Movies" fetchUrl ={requests.fetchActionMovies} />
+      <Row title ="Comedy Movies" fetchUrl ={requests.fetchComedyMovies} />
+      <Row title ="Horror Movies" fetchUrl ={requests.fetchHorrorMovies} />
+      <Row title ="Romance Movies" fetchUrl ={requests.fetchRomanceMovies} />
+      <Row title ="Documentaries Movies" fetchUrl ={requests.fetchDocumentaries} />
     </div>
   );
 }
--- netflix/src/Row.js
+++ netflix/src/Row.js
@@ -2,26 +2,28 @@ import React,{useState,useEffect} from 'react';
 import './Row.css';
 import axios from './axios';
 
-const base_url="https://image.tmdb.org/t/p/orginal/";
+const base_url="https://image.tmdb.org/t/p/original";
 function Row({title,fetchUrl}) {
     const [movies, setMovies] = useState([]);
      useEffect(() => {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
-            console.log(request);
+            // console.log(request);
             setMovies(request.data.results);
             return request;
         }
         fetchData();
      }, [fetchUrl]);
 
+     console.log(movies);
+
   return (
     <div className="row">
       <h1>{title}</h1>
-      <div className="row__posters">
+      <div className="row_posters">
         {movies.map((movie) => (
-            <img className="row__poster" src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
-        ))}
+            <img className="row_poster" src={`${base_url} ${movie.poster_path}`} alt={movie.name}/>
+        ))};
       </div>
     </div>
   )
