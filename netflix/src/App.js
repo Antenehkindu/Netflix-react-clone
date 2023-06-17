43f0f7794d9edd38d47d3c27fa8eb024b6375c73
Anteneh kindu <126372714+Antenehkindu@users.noreply.github.com>
2023-06-18 00:03:00 +0300

    mapping image

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
+import Banner from './Banner';
 import requests from './requests';
 import Row from './Row';
 
 function App() {
   return (
     <div className="App">
-      <h1>Evangadi Familly</h1>
-      <Row title ="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Banner />
+      <Row title ="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} 
+      isLargeRow/>
       <Row title ="Trending Now" fetchUrl ={requests.fetchTrending} />
       <Row title ="Top Rated" fetchUrl ={requests.fetchTopRatedMovies} />
       <Row title ="Action Movies" fetchUrl ={requests.fetchActionMovies} />
---
+++ netflix/src/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Nav.css";
+function Nav() {
+  return (
+   <div className="nav">
+    <img className="nav__logo"
+    src = ""
+    alt = "Netflix Logo" />
+
+    <img className="nav__avater"
+    src = "https://i.pinimg.com/originals/0d/dc/ca/oddccae723d85a703b798"
+    alt = "Avatar Logo" />
+    
+   </div>
+  )
+}
+
+export default Nav
